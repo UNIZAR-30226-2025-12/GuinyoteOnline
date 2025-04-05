6799e624d80f16b87fb18b6c6afb1728d62642ac
hectorcaver <hector@DESKTOP-AB9ORRS.>
2025-04-05 21:46:08 +0200

    Eliminados ficheros basura

--- navegacion_web/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// Temporarily remove <React.StrictMode> for testing
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
